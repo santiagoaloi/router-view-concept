835466394d95f179fb64755959ef737f410c5f5b
Santiago Aloi <santiagoaloi@gmail.com>
2023-05-19 12:41:15 +0200

    updates

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,25 +7,26 @@
     <VCardTitle class="text-h5 font-weight-bold my-8"> Sign in </VCardTitle>
     <VCardSubtitle>Credentials are admin / vuetify </VCardSubtitle>
     <VCardText>
-      <v-form> </v-form>
-      <v-text-field v-model="loginForm.account" label="Account" />
-      <v-text-field
-        type="password"
-        v-model="loginForm.password"
-        label="Password"
-      />
-      <div class="d-flex justify-end">
-        <v-btn
-          :loading="loading"
-          @click="login()"
-          color="indigo"
-          :ripple="false"
-          >Continue</v-btn
-        >
-      </div>
+      <v-form>
+        <v-text-field v-model="loginForm.account" label="Account" />
+        <v-text-field
+          type="password"
+          v-model="loginForm.password"
+          label="Password"
+        />
+        <div class="d-flex justify-end">
+          <v-btn
+            :loading="loading"
+            @click="login()"
+            color="indigo"
+            :ripple="false"
+            >Continue</v-btn
+          >
+        </div>
+      </v-form>
     </VCardText>
   </v-card>
-  <isLoggedIn v-else />
+  <isLoggedIn v-if="!loading && appStore.isLoggedIn" />
 </template>
 <script setup>
 import { reactive, ref } from "vue";
@@ -45,9 +46,11 @@ const appStore = useAppStore();
 
 async function login() {
   loading.value = true;
+
   const { success } = await appStore.signInWithPassword({
     ...loginForm,
   });
+
   if (success) {
     appStore.isLoggedIn = true;
     router.push("/secure");
